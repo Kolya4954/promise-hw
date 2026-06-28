76a73a162ccdb30b51059dae5993300d6acaefb4
Коля <135145397+Kolya4954@users.noreply.github.com>
2026-06-28 13:56:25 +0300

    final fix

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ const delay = (ms) => {
 const logger = (time) => console.log(`Resolved after ${time}ms`);
 
 // Виклич функції для перевірки
+
+
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 
